b0850d17b0e161c28df570d231ac40461834a758
user1 <user1@DESKTOP-1E009G1>
2023-02-07 14:30:07 +0900

    Run

--- src/com/jsh/run/Run.java
+++ src/com/jsh/run/Run.java
@@ -1,6 +1,7 @@
 package com.jsh.run;
 
 import com.jsh.study.A_TicTacToe;
+import com.jsh.study.B_ArrayStudy;
 
 public class Run {
 	public static void main(String[] args) {
@@ -10,5 +11,9 @@ public class Run {
 		}catch(Exception e) {
 			System.out.println(e);
 		}
+		
+		// ArrayStudy
+		B_ArrayStudy b = new B_ArrayStudy();
+		b.study01();
 	}
 }
