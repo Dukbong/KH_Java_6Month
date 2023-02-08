9c69fcfe058f50f9a354bfdc933d0d8f5e534f0d
user1 <user1@DESKTOP-1E009G1>
2023-02-08 19:55:43 +0900

    RUN

--- src/com/jsh/run/Run.java
+++ src/com/jsh/run/Run.java
@@ -4,10 +4,14 @@ import com.jsh.study.A_TicTacToe;
 import com.jsh.study.B_ArrayStudy;
 import com.jsh.study.C_Singleton;
 import com.jsh.study.F_TwoDimensionalArray;
+import com.jsh.study.G_ObjectStudy;
+
+
 
 public class Run {
+	
 	public static void main(String[] args) {
-		// TicTacToe Game Start
+		 // TicTacToe Game Start
 		try {
 			A_TicTacToe.GameStart();
 		}catch(Exception e) {
@@ -28,5 +32,36 @@ public class Run {
 		
 		F_TwoDimensionalArray f = new F_TwoDimensionalArray();
 		f.study();
+		
+		G_ObjectStudy g = new G_ObjectStudy();
+		g.show();
 	}
 }
+
+
+// 생성자 공부
+//class parent{
+//	int x;
+//	int y;
+//	parent(int x, int y){
+//		this.x = x;
+//		this.y = y;
+//	}
+//
+//}
+//
+//class child extends parent{
+//	int z;
+//	child(int x, int y, int z){
+//		super(x,y);
+//		this.z = z;
+//	}
+//	child(){
+//		
+//	}
+//	
+//	child(int z){
+//		this();
+//	}
+//	
+//}
