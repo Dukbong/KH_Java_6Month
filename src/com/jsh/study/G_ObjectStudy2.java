5cb42c1e72ac4923714a3c7a0c6c848ca348087f
user1 <user1@DESKTOP-1E009G1>
2023-02-09 20:17:10 +0900

    ObjectStudy

---
+++ src/com/jsh/study/G_ObjectStudy2.java
@@ -0,0 +1,43 @@
+package com.jsh.study;
+
+public class G_ObjectStudy2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c = new Car();
+		Fire f = new Fire();
+		
+		SuperCar s = new SuperCar();
+		c = s;
+		test(c);
+
+	}
+	
+	static void test(Car c) {
+		if(c instanceof Fire) { // A instanceof B --> A가 실제로 가르키는 객체가 B or B의 자식이냐 묻고 맞으면 true 아니면 false 많이 헷갈림
+			System.out.println("true");
+			
+		}else {
+			System.out.println("false");
+		}
+	}
+
+}
+
+class Car{
+	void go() {
+		System.out.println("brrrr");
+	}
+}
+
+class Fire extends Car{
+	void water() {
+		System.out.println("water!!");
+	}
+}
+
+class SuperCar extends Fire{
+	void superr() {
+		System.out.println("super!!");
+	}
+}
