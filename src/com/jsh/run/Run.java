55ebda8cf757a59be168c3c1a43a3e8bd92aa906
user1 <user1@DESKTOP-1E009G1>
2023-02-15 09:57:41 +0900

    RUN

--- src/com/jsh/run/Run.java
+++ src/com/jsh/run/Run.java
@@ -1,40 +1,38 @@
 package com.jsh.run;
 
-import com.jsh.study.A_TicTacToe;
-import com.jsh.study.B_ArrayStudy;
-import com.jsh.study.C_Singleton;
-import com.jsh.study.F_TwoDimensionalArray;
-import com.jsh.study.G_ObjectStudy;
+import com.jsh.study.J_Collection;
 
 
 
 public class Run {
 	
 	public static void main(String[] args) {
+		J_Collection j = new J_Collection();
+		j.Test();
 		 // TicTacToe Game Start
-		try {
-			A_TicTacToe.GameStart();
-		}catch(Exception e) {
-			System.out.println(e);
-		}
-		
-		// ArrayStudy
-		B_ArrayStudy b = new B_ArrayStudy();
-		b.study01();
-		
-		
-		// C_Singleton
-		C_Singleton s1 = C_Singleton.getInstance();
-		C_Singleton s2 = C_Singleton.getInstance();
-		
-		s1.call("A_TicTacToe.java");
-		s2.call("B_ArrayStudy.java");
-		
-		F_TwoDimensionalArray f = new F_TwoDimensionalArray();
-		f.study();
-		
-		G_ObjectStudy g = new G_ObjectStudy();
-		g.show();
+//		try {
+//			A_TicTacToe.GameStart();
+//		}catch(Exception e) {
+//			System.out.println(e);
+//		}
+//		
+//		// ArrayStudy
+//		B_ArrayStudy b = new B_ArrayStudy();
+//		b.study01();
+//		
+//		
+//		// C_Singleton
+//		C_Singleton s1 = C_Singleton.getInstance();
+//		C_Singleton s2 = C_Singleton.getInstance();
+//		
+//		s1.call("A_TicTacToe.java");
+//		s2.call("B_ArrayStudy.java");
+//		
+//		F_TwoDimensionalArray f = new F_TwoDimensionalArray();
+//		f.study();
+//		
+//		G_ObjectStudy g = new G_ObjectStudy();
+//		g.show();
 	}
 }
 
